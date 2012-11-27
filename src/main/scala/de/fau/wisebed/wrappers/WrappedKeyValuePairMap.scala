89959e5d8362979f515f931b900a34ae34e2ad4f
Moritz 'Morty' Strübe <morty@gmx.net>
2012-11-27 18:27:08 +0100

    Missed adding WrappedKeyValuePairMap

---
+++ src/main/scala/de/fau/wisebed/wrappers/WrappedKeyValuePairMap.scala
@@ -0,0 +1,29 @@
+package de.fau.wisebed.wrappers
+
+import scala.collection._
+import eu.wisebed.api.common.KeyValuePair
+import scala.collection.JavaConversions._
+
+class WrappedKeyValuePairMap(_ls:java.util.List[KeyValuePair]) extends mutable.Map[String, String] {
+    def ls = asScalaBuffer(_ls)
+	
+	def get(key: String): Option[String] = ls.find(_.getKey == key) map (_.getValue)
+	def iterator: Iterator[(String, String)] = ls.map(x => {x.getKey -> x.getValue}).iterator
+	
+	def += (kv: (String, String)) = { 
+		remove(kv._1)
+		val kvp = new KeyValuePair 
+		kvp.setKey( kv._1)
+		kvp.setValue(kv._2)
+		ls.append(kvp)
+		WrappedKeyValuePairMap.this 
+	}
+	
+	def -= (key: String) = { remove(key); WrappedKeyValuePairMap.this }
+
+	private def remkey(key:String)  {
+		ls.filter(_.getKey == key).foreach(ls -= _) 
+
+	}
+	
+}
