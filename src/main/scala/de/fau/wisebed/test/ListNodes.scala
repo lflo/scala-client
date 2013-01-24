f08d295d48c9069b542f52e164e5dff0c6fa3418
Florian Lukas <florian.lukas@gmail.com>
2013-01-24 09:25:08 +0100

    Reimplement -- on List to fix compile error

--- src/main/scala/de/fau/wisebed/test/ListNodes.scala
+++ src/main/scala/de/fau/wisebed/test/ListNodes.scala
@@ -16,7 +16,7 @@ object ListNodes {
 		val log = LoggerFactory.getLogger(getClass)
 	
 		TH.init(2)
-		val inact = TH.motes -- TH.activemotes
+		val inact = TH.motes.filter(!TH.activemotes.contains(_))
 		log.info("Aktive Nodes  : " +  TH.activemotes.mkString(", "))
 		log.info("Inaktive Nodes: " +  inact.mkString(", "))
 		
