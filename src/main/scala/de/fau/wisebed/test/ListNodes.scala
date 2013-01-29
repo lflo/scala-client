f32666f80a3c57f52d222731584ba5b042c96f62
Florian Lukas <florian.lukas@gmail.com>
2013-01-29 23:06:24 +0100

    Remove eclipse/, call TH.finish in ListNodes.scala

--- src/main/scala/de/fau/wisebed/test/ListNodes.scala
+++ src/main/scala/de/fau/wisebed/test/ListNodes.scala
@@ -5,20 +5,16 @@ import de.fau.wisebed.messages.MessageWaiter
 import de.uniluebeck.itm.tr.util.Logging
 import org.apache.log4j.Level
 
-
-
 object ListNodes {
-	
-
-	
+	val log = LoggerFactory.getLogger(getClass)
 		
 	def main(args: Array[String]) {
-		val log = LoggerFactory.getLogger(getClass)
-	
 		TH.init(2)
+
 		val inact = TH.motes.filter(!TH.activemotes.contains(_))
 		log.info("Aktive Nodes  : " +  TH.activemotes.mkString(", "))
 		log.info("Inaktive Nodes: " +  inact.mkString(", "))
 		
+		TH.finish
 	}
 }
